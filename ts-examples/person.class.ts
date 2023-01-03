5b1afa9b13217b60196617dd6e736b77ac0aa17c
Walter McCue <walter.mccue@gmail.com>
2023-01-02 23:02:02 -0500

    Assignment 1.4

---
+++ ts-examples/person.class.ts
@@ -0,0 +1,15 @@
+import { IPerson } from "./person.interface";
+
+class Person implements IPerson {
+    
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
+
+let myName = new Person("Walter", "McCue");
+console.log(`My name is ${myName.firstName} ${myName.lastName}.`);
