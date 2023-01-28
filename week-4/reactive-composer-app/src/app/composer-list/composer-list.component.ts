19eb930ad6d376c1cbad0bab6eff6c0e3e2ae85c
Walter McCue <walter.mccue@gmail.com>
2023-01-27 23:03:43 -0500

    web 425 exercise4.3

--- week-4/reactive-composer-app/src/app/composer-list/composer-list.component.ts
+++ week-4/reactive-composer-app/src/app/composer-list/composer-list.component.ts
@@ -8,6 +8,8 @@
 import { Component, OnInit } from '@angular/core';
 import { IComposer } from '../composer.interface';
 import { ComposerService } from '../composer.service';
+import { FormControl } from "@angular/forms";
+import { debounceTime } from 'rxjs/operators';
 
 
 @Component({
@@ -21,11 +23,19 @@ import { ComposerService } from '../composer.service';
 export class ComposerListComponent implements OnInit {
 
   composers: Array<IComposer>;
+  txtSearchControl = new FormControl('');
 
   constructor(private composerService: ComposerService) {
     this.composers = this.composerService.getComposers();
+    this.txtSearchControl.valueChanges.pipe(debounceTime(500)).subscribe(val => this.filterComposers(val!));
   }
+
   ngOnInit(): void {
 
   }
+
+  filterComposers(name: string) {
+    alert(name);
+  }
+
 }
